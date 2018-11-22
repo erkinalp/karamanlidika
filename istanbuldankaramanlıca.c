51a6a23429754f90dde33b6c773eb1672dc42544
Erkin Alp Güney <erkinalp9035@gmail.com>
2018-11-22 22:45:06 +0300

    Fixed fallthrough

--- istanbuldankaramanlıca.c
+++ istanbuldankaramanlıca.c
@@ -22,31 +22,31 @@ int main() {
 	char in[2]={0},char out[3]={0};
 	while (fread(in,1,2,stdin)==2) {
 		switch(in[0]){
-		case 'A': puts("Α"); // A
-		case 'B': puts("Π̈́"); // B
-		case 'C': puts("ΤΖ"); // C
-		case 'D': puts("Τ̈́"); // D
-		case 'E': puts("Ε"); // E
-		case 'F': puts("Φ"); // F
-		case 'G': puts("Κ̈́") // G
-		case 'H': puts("Χ"); // H
-		case 'I': puts("Ι");  // I
-		case 'J': puts("ΔΖ"); // J
-		case 'K': case 'Q': puts("Κ"); // K - Q
-		case 'L': puts("Λ"); // L 
-		case 'M': puts("Μ"); // M
-		case 'N': puts("Ν"); // N
-		case 'O': puts("Ο"); // O
-		case 'P': puts("Π"); // P
-		case 'R': puts("Ρ"); // R
-		case 'S': puts("Σ"); // S
-		case 'T': puts("Τ") // T
-		case 'U': puts("Υ"); // U
-		case 'V': puts("Β"); // V
-		case 'W': puts("ϒ"); // W
-		case 'X': puts("ΚΣ"); // X
-		case 'Y': puts("Γ"); // Y
-		case 'Z': puts("Ζ");  // Z
+		case 'A': puts("Α"); break;// A
+		case 'B': puts("Π̈́"); break;// B
+		case 'C': puts("ΤΖ"); break; // C
+		case 'D': puts("Τ̈́"); break; // D
+		case 'E': puts("Ε"); break;// E
+		case 'F': puts("Φ"); break;// F
+		case 'G': puts("Κ̈́") break;// G
+		case 'H': puts("Χ"); break;// H
+		case 'I': puts("Ι"); break; // I
+		case 'J': puts("ΔΖ"); break; // J
+		case 'K': case 'Q': puts("Κ"); break; // K - Q
+		case 'L': puts("Λ"); break; // L 
+		case 'M': puts("Μ"); break; // M
+		case 'N': puts("Ν"); break; // N
+		case 'O': puts("Ο"); break; // O
+		case 'P': puts("Π"); break; // P
+		case 'R': puts("Ρ"); break; // R
+		case 'S': puts("Σ"); break; // S
+		case 'T': puts("Τ") break; // T
+		case 'U': puts("Υ"); break; // U
+		case 'V': puts("Β"); break; // V
+		case 'W': puts("ϒ"); break; // W
+		case 'X': puts("ΚΣ"); break; // X
+		case 'Y': puts("Γ"); break; // Y
+		case 'Z': puts("Ζ"); break; // Z
 		case 'a':  // a
 		case 'b':  // b
 		case 'c':  // c
